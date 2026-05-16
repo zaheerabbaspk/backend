514f8a246df2fe11458dc079947bb7c59aa82eae
Zaheer Abbas <zaheerstudioz@gmail.com>
2026-05-16 16:13:54 +0500

    .

--- src/sockets/playerHandlers.js
+++ src/sockets/playerHandlers.js
@@ -1,15 +1,32 @@
 const gameEngine = require('../game/GameEngine');
+const cardBetEngine = require('../game/CardBetEngine');
 
 module.exports = (io, socket) => {
     console.log(`Player connected: ${socket.id}`);
 
-    // Send current state to newly connected player
+    // Send current Crash state
     socket.emit('gameStateUpdate', {
         state: gameEngine.state,
         multiplier: parseFloat(gameEngine.multiplier.toFixed(2)),
         timeLeft: gameEngine.timeLeft,
         roundId: gameEngine.roundId || 'round_' + Date.now()
     });
+
+    // Send current CardBet state
+    socket.emit('cardBet_StateUpdate', {
+        state: cardBetEngine.state,
+        timeLeft: cardBetEngine.timeLeft,
+        roundId: 'cb_' + Date.now()
+    });
+
+    // If game is in REVEALING state, send the current result too
+    if (cardBetEngine.state === 'REVEALING') {
+        socket.emit('cardBet_Result', {
+            winningHandId: cardBetEngine.winningHandId,
+            hands: cardBetEngine.hands,
+            roundId: 'cb_' + Date.now()
+        });
+    }
 
     socket.on('placeBet', (betData) => {
         console.log(`Bet placed by ${socket.id}:`, betData);
